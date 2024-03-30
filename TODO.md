a514e82ba8cff53f17bd12ef37d2a545f11a741c
Gary Wallen <garycwallen@gmail.com>
2024-03-29 22:30:12 -0400

    Updated TODO & updated branding

--- TODO.md
+++ TODO.md
@@ -2,4 +2,38 @@
 1. ~~Setup MongoDB prisma~~
 1. ~~Setup Google auth with NextAuth~~
 1. Define MongoDB Schema
-1. 
+1. Setup Navbar
+  - Logo
+  - User Image
+  - User Name
+  - Signout
+1. [UI] Add Transactions
+  - Expense or Income (Default to Income)
+  - Amount
+  - Location
+  - Date (Optional, current is default, but allow for future planning)
+  - Note (Optional)
+  - Should be editable
+  - Should be deleteable (Soft delete, with confirmation)
+1. [Backend] Add Transcations
+  - Type (Income/Expense)
+  - Amount ($123.45)
+  - Location (Target)
+  - Date (Datetime)
+  - Note (Text/String)
+  - User
+- [UI] Features
+  - Daily/Weekly/Monthly Recap
+    - Locations & Income/Expense
+  - List past X transcations sortable by date/amount
+  - Clickable on `Location` to view spending per `Location`
+      - Last X transcations
+      - Total sum
+      - Same chart availability
+  - Charts by type
+    - Day over day
+    - Week over week
+    - Month over month
+    - Year over year
+  - Pending Transcations
+  - Total Balance
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  const hello = await api.post.hello({ text: "from Ledger!" });
   const session = await getServerAuthSession();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          Family <span className="text-[hsl(280,100%,70%)]">Ledger</span>
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
@@ -40,7 +40,7 @@ export default async function Home() {
         </div>
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
+            {hello ? hello.greeting : "Loading..."}
           </p>
 
           <div className="flex flex-col items-center justify-center gap-4">
