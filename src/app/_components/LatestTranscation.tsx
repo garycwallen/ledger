e37f3d217e9bc7c3dc334d2242d5c22d14a197ce
Gary Wallen <garycwallen@gmail.com>
2024-04-03 21:59:15 -0400

    Added currency formatter to main page and conditional coloring

--- src/app/_components/LatestTranscation.tsx
+++ src/app/_components/LatestTranscation.tsx
@@ -1,5 +1,6 @@
 import { getServerAuthSession } from "@/server/auth";
 import { api } from "@/trpc/server";
+import { currencyFormatter } from "@/lib/utils";
 
 export default async function LatestTranscation() {
   const session = await getServerAuthSession();
@@ -7,19 +8,20 @@ export default async function LatestTranscation() {
 
   const latestTranscation = await api.transcation.getLatest();
 
-  const purchase =
-    latestTranscation?.type +
-    " $" +
-    latestTranscation?.amount +
-    " at " +
-    latestTranscation?.location +
-    " on " +
-    latestTranscation?.createdAt.toLocaleDateString();
-
   return (
     <div className="w-full max-w-xs">
       {latestTranscation ? (
-        <p>Most recent transcation: {purchase}</p>
+        <p>
+          Most recent transcation:{" "}
+          {latestTranscation.type === "Expense" ? (
+            <span className="text-red-500">{latestTranscation?.type}</span>
+          ) : (
+            <span className="text-green-500">{latestTranscation?.type}</span>
+          )}{" "}
+          {currencyFormatter(latestTranscation?.amount)} at{" "}
+          {latestTranscation?.location} on{" "}
+          {latestTranscation?.createdAt.toLocaleDateString()}
+        </p>
       ) : (
         <p>You have no expenses yet.</p>
       )}
