d0e4b30ce7720229fb9050edf361d15cc12ebcd7
Gary Wallen <garycwallen@gmail.com>
2024-04-05 20:39:32 -0400

    Cleaned up UI on all transcations page

--- src/app/transcations/page.tsx
+++ src/app/transcations/page.tsx
@@ -4,13 +4,13 @@ import TotalBalance from "../_components/TotalBalance";
 export default function TranscationsPage() {
   return (
     <>
-      <main className="flex flex-col items-center text-white">
+      <main className="container mx-auto max-w-2xl px-6">
+        <TotalBalance />
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[4rem]">
             All <span className="text-[hsl(280,100%,70%)]">Transcations</span>
           </h1>
           {/* Our Balance + SUM section */}
-          <TotalBalance />
         </div>
       </main>
       {/* Table of All Transcation */}
