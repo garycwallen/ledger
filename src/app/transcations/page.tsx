6338dd1f61eedd3f32cae29038a703eb8a44f14a
Gary Wallen <garycwallen@gmail.com>
2024-04-05 20:40:10 -0400

    Updated comment

--- src/app/transcations/page.tsx
+++ src/app/transcations/page.tsx
@@ -5,12 +5,12 @@ export default function TranscationsPage() {
   return (
     <>
       <main className="container mx-auto max-w-2xl px-6">
+        {/* Our Balance + SUM section */}
         <TotalBalance />
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[4rem]">
             All <span className="text-[hsl(280,100%,70%)]">Transcations</span>
           </h1>
-          {/* Our Balance + SUM section */}
         </div>
       </main>
       {/* Table of All Transcation */}
