4139e62028cfed7bfe74473dfbc8576a87325e3b
Gary Wallen <garycwallen@gmail.com>
2024-04-05 20:46:03 -0400

    Simplified table and adding extra padding

--- src/app/_components/AllTranscations.tsx
+++ src/app/_components/AllTranscations.tsx
@@ -1,60 +1,20 @@
 import { getServerAuthSession } from "@/server/auth";
-import { api } from "@/trpc/server";
-import { currencyFormatter } from "@/lib/utils";
+import TranscationsTable from "@/app/_components/TranscationsTable";
 
 export default async function AllTranscations() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const transcationscolumns = ["Amount", "Location", "Date"];
-
-  const allTransactions = await api.transcation.getAll();
   return (
     <section className="min-h-screen px-4 text-gray-600 antialiased">
-      <div className="flex h-full flex-col justify-center">
-        <div className="mx-auto w-full max-w-2xl rounded-lg border border-gray-200 bg-white shadow-lg">
-          <header className="border-b border-gray-100 px-5 py-4">
+      <div className="flex h-full flex-col justify-center py-6">
+        <div className="mx-auto w-full max-w-2xl rounded-lg border border-gray-200 bg-white py-6 shadow-lg">
+          <header className="border-b border-gray-100 px-5">
             <h2 className="font-semibold text-gray-800">All Transcations</h2>
           </header>
           <div className="p-3">
             <div className="overflow-x-auto border">
-              <table className="w-full table-auto">
-                <thead className="bg-gray-200 text-xs font-semibold uppercase text-gray-400">
-                  <tr>
-                    {transcationscolumns.map((columns) => (
-                      <th className="whitespace-nowrap p-2" key="id">
-                        <div className="text-left font-semibold">{columns}</div>
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                {allTransactions?.map((transcation) => (
-                  <tbody
-                    className="divide-y divide-gray-100 text-sm"
-                    key={transcation.id}
-                  >
-                    <tr className="hover:bg-gray-100 dark:hover:bg-gray-200">
-                      <td className="whitespace-nowrap p-2">
-                        {transcation.type === "Expense" ? (
-                          <div className="text-left font-medium text-red-500">
-                            {currencyFormatter(transcation.amount)}
-                          </div>
-                        ) : (
-                          <div className="text-left font-medium text-green-500">
-                            {currencyFormatter(transcation.amount)}
-                          </div>
-                        )}
-                      </td>
-                      <td className="whitespace-nowrap p-2">
-                        {transcation.location}
-                      </td>
-                      <td className="whitespace-nowrap p-2">
-                        {transcation.createdAt.toLocaleDateString()}
-                      </td>
-                    </tr>
-                  </tbody>
-                ))}
-              </table>
+              <TranscationsTable />
             </div>
           </div>
         </div>
---
+++ src/app/_components/TranscationsTable.tsx
@@ -0,0 +1,46 @@
+import { api } from "@/trpc/server";
+import { currencyFormatter } from "@/lib/utils";
+
+export default async function TranscationsTable() {
+  const transcationscolumns = ["Amount", "Location", "Date"];
+
+  const allTransactions = await api.transcation.getAll();
+
+  return (
+    <table className="w-full table-auto">
+      <thead className="bg-gray-200 text-xs font-semibold uppercase text-gray-400">
+        <tr>
+          {transcationscolumns.map((columns) => (
+            <th className="whitespace-nowrap p-2" key="id">
+              <div className="text-left font-semibold">{columns}</div>
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className="divide-y divide-gray-100 text-sm">
+        {allTransactions?.map((transcation) => (
+          <tr
+            className="hover:bg-gray-100 dark:hover:bg-gray-200"
+            key={transcation.id}
+          >
+            <td className="whitespace-nowrap p-2">
+              {transcation.type === "Expense" ? (
+                <div className="text-left font-medium text-red-500">
+                  {currencyFormatter(transcation.amount)}
+                </div>
+              ) : (
+                <div className="text-left font-medium text-green-500">
+                  {currencyFormatter(transcation.amount)}
+                </div>
+              )}
+            </td>
+            <td className="whitespace-nowrap p-2">{transcation.location}</td>
+            <td className="whitespace-nowrap p-2">
+              {transcation.createdAt.toLocaleDateString()}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
