39857f3632b14b3931cb20b8bdc3e3c89fb227a5
Gary Wallen <garycwallen@gmail.com>
2024-04-11 15:23:19 -0700

    Updated spelling on LatestTransaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { getServerAuthSession } from "@/server/auth";
-import LatestTranscation from "@/app/_components/LatestTransaction";
+import LatestTransaction from "@/app/_components/LatestTransaction";
 import TotalBalance from "@/app/_components/TotalBalance";
 import CreateTransaction from "@/app/_components/CreateTransaction";
 
@@ -21,7 +21,7 @@ export default async function Home() {
                 >
                   View All Transactions
                 </Link>
-                <LatestTranscation />
+                <LatestTransaction />
                 <CreateTransaction />
               </>
             ) : (
